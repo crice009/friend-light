0a4f1e09bb19847ca2058cf97e20c82c15c580e2
Corey Rice <crice009@gmail.com>
2024-02-22 21:56:54 -0500

    connect secret file & notes in secret.example

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <secret.h>
 
 // put function declarations here:
 int myFunction(int, int);
--- src/secret.example.h
+++ src/secret.example.h
@@ -1,2 +1,9 @@
+/*
+ *
+ *   This is an example of the secret.h file that you need to create in the src folder
+ *   The secret.h file is not included in the repository, so you need to create it yourself
+ *   --> Use this file as a template and replace the placeholders with your own data
+ *  
+ */
 #define WIFI_SSID="your_ssid"
 #define WIFI_PASS="your_password"

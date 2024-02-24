954475ad4fc2b3c97b67b65d0aa692b8e6672368
Corey Rice <crice009@gmail.com>
2024-02-23 19:59:23 -0500

    just blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println();
   Serial.println("Starting...");
 }
@@ -9,8 +9,23 @@ void setup() {
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
   Serial.println("LED on");
-  delay(500);
+  delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
   Serial.println("LED off");
   delay(500);
+
+  #ifdef RGB_BUILTIN
+  Serial.println("LED is RED");
+  neopixelWrite(RGB_BUILTIN,255,  0,  0); // Red
+  delay(1000);
+  Serial.println("LED is GREEN");
+  neopixelWrite(RGB_BUILTIN,  0,255,  0); // Green
+  delay(1000);
+  Serial.println("LED is BLUE");
+  neopixelWrite(RGB_BUILTIN,  0,  0,255); // Blue
+  delay(1000);
+  Serial.println("LED off");
+  neopixelWrite(RGB_BUILTIN,0,0,0); // Off / black
+  delay(500);
+  #endif
 }

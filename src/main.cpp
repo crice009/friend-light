80a7847594ae096c6b00a884f3824354f375237d
Corey Rice <crice009@gmail.com>
2024-02-25 09:57:09 -0500

    simplify main

--- src/main.cpp
+++ src/main.cpp
@@ -86,8 +86,7 @@ void onMqttUnsubscribe(uint16_t packetId) {
   Serial.println(packetId);
 }
 
-#include <cstring>
-
+#include "../lib/mqttActions/colorMsg.cpp"
 void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
   Serial.println("Message received.");
   Serial.print("  topic: ");
@@ -96,48 +95,9 @@ void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties 
   Serial.println(payload);
 
   // Check if the topic is the color topic
-  if (strcmp(topic, "test/lol/color") != 0) {
-    return;
+  if (strcmp(topic, "test/lol/color") == 0) {
+    onColorMsg(payload);
   }
-
-  Serial.println("message is in color category.");
-
-  
-  // Check if the payload is a valid hex color code
-  size_t payloadLen = strlen(payload);
-  // Check if the payload is a valid hex color code
-  bool isValidHex = true;
-  for (size_t i = 0; i < payloadLen; i++) {
-    if (!isxdigit(payload[i])) {
-      isValidHex = false;
-      break;
-    }
-  }
-
-  // Return the color code, if valid or a default color
-  const char * hexColor = "000000";
-  if (isValidHex) {
-    hexColor = payload;
-  }
-
-  char r[3] = {hexColor[0], hexColor[1], '\0'};
-  char g[3] = {hexColor[2], hexColor[3], '\0'};
-  char b[3] = {hexColor[4], hexColor[5], '\0'};
-
-  int rgb[3];
-  rgb[0] = strtol(r, NULL, 16);
-  rgb[1] = strtol(g, NULL, 16);
-  rgb[2] = strtol(b, NULL, 16);
-
-  Serial.print("  r: ");
-  Serial.println(rgb[0]);
-  Serial.print("  g: ");
-  Serial.println(rgb[1]);
-  Serial.print("  b: ");
-  Serial.println(rgb[2]);
-  #ifdef RGB_BUILTIN
-  neopixelWrite(RGB_BUILTIN,rgb[0],rgb[1],rgb[2]);
-  #endif
 }
 
 void onMqttPublish(uint16_t packetId) {
